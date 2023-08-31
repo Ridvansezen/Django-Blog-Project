9f78983f4e28a003ebdcde5a73e3d71f8d8dad36
Ridvansezen <sezenridvan309@gmail.com>
2023-08-31 19:50:15 +0300

    commit

--- blog_project/article/models.py
+++ blog_project/article/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    author = models.ForeignKey("auth.User", on_delete= models.CASCADE)
+    title = models.CharField(max_length= 50)
+    content = models.TextField(max_length=500)
+    created_date = models.DateTimeField(auto_now_add=True)
