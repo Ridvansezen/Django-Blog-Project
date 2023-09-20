0b5b1b1487cc243af254271de8e39ca32cef53a1
Ridvansezen <sezenridvan309@gmail.com>
2023-09-21 01:36:36 +0300

    new commit

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -27,8 +27,6 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['3.71.77.198']
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
 # Application definition
@@ -126,16 +124,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-
+STATICFILES_FINDERS = (
+'django.contrib.staticfiles.finders.FileSystemFinder',
+'django.contrib.staticfiles.finders.AppDirectoriesFinder'
+)
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,"static"),
+STATIC_ROOT = "/home/Django-Blog-Project/blog_project/staticfiles/"
+STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+     '/home/Django-Blog-Project/blog_project/staticfiles/',
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+MEDIA_ROOT = '/home/Django-Blog-Project/blog_project/media/'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
