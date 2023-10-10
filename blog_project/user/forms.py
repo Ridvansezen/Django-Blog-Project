726b26c390e563fef36dd56a8d18b8f5eb0d3d4e
Ridvansezen <sezenridvan309@gmail.com>
2023-10-10 23:12:41 +0300

    [FE-1] Character limitations were applied to username and password inputs.

--- blog_project/user/forms.py
+++ blog_project/user/forms.py
@@ -5,9 +5,9 @@ class LoginForm(forms.Form):
     password = forms.CharField(label="Parola",widget=forms.PasswordInput)
 
 class RegisterForm(forms.Form):
-    username = forms.CharField(max_length=50, label = "Kullanıcı adı")
-    password = forms.CharField(max_length = 50, label= "Parola" , widget=forms.PasswordInput)
-    confirm_password = forms.CharField(max_length=50, label="Parolayı doğrulayın", widget=forms.PasswordInput)
+    username = forms.CharField(min_length=4,max_length=18, label = "Kullanıcı adı") # Min & Max lengths applied to forms
+    password = forms.CharField(min_length=6,max_length = 18, label= "Parola" , widget=forms.PasswordInput) # Min & Max lengths applied to forms
+    confirm_password = forms.CharField(min_length=6,max_length=18, label="Parolayı doğrulayın", widget=forms.PasswordInput) # Min & Max lengths applied to forms
 
     def clean(self):
         username = self.cleaned_data.get("username")
