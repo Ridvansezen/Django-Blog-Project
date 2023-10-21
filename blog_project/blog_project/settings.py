c6355763d61b431e1b74899d46a87f5ef88eae41
Ridvansezen <sezenridvan309@gmail.com>
2023-10-21 19:19:25 +0300

    [FE-1] 404 Page not found page customized.

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-w5(v)jkq+x0b(@ju=1!%thd7zt8&e#hdb*lte)@2gvh40j8jl%"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
